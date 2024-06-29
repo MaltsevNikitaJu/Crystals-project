1af7b498fab0d6a2d30d7909ae68008fabfe69e4
Maltsev Nikita <MaltsevNikitaJu@yandex.ru>
2024-06-29 20:55:59 +0500

    server issue resolved

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -10,11 +10,9 @@ const db = knex(dbConfig);
 db.raw('SELECT 1')
   .then(() => {
     console.log('Connected to the database successfully');
-    process.exit(0);
   })
   .catch(err => {
     console.error('Unable to connect to the database:', err);
-    process.exit(1);
   });
 
 module.exports = db;
