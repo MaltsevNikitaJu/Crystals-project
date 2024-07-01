403faf2f12f78ff92019c38d427740d5e61b414d
Maltsev Nikita <MaltsevNikitaJu@yandex.ru>
2024-07-01 17:20:47 +0500

    some changes

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -5,8 +5,27 @@ const { createUser, findUserByEmail } = require("../models/userModel");
 const register = async (req, res) => {
   try {
     const { email, password, name } = req.body;
+
+    if (!email || !password || !name) {
+      return res.status(400).json({ error: "Все поля обязательны" });
+    }
+
+    const exisitngUser = await findUserByEmail(email);
+    if (exisitngUser) {
+      return res
+        .status(400)
+        .json({ error: "Пользователь с таким email уже существует" });
+    }
+
     const hashedPassword = await bcrypt.hashPassword(password);
     const newUser = await createUser({ email, password: hashedPassword, name });
+
+    if (!newUser) {
+      return res
+        .status(500)
+        .json({ error: "Ошибка при создании пользователя" });
+    }
+
     const token = jwt.generateToken(newUser);
     res.status(201).json({ user: newUser, token });
   } catch (error) {
@@ -20,10 +39,12 @@ const login = async (req, res) => {
     const user = await findUserByEmail(email);
     if (!user || !(await bcrypt.comparePassword(password, user.password))) {
       return res.status(401).json({ error: "Неверный логин или пароль" });
-    }
-    const token = jwt.generateToken(user);
-    res.status(200).json({ user, token });
+    } 
+      const token = jwt.generateToken(user);
+      res.status(200).json({message:"успешный вход", user, token });
+    
   } catch (error) {
+    console.log(error)
     res.status(500).json({ error: "Ошибка авторизации" });
   }
 };
